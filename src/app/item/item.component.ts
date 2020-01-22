2341144ecdee8b31f661fdb062bfeabffb88c463
Verenasworld <verena.felizidas@gmail.com>
2020-01-22 22:01:16 +0100

    debugg error

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'todo-item',
-  templateUrl: `
-  {{ todoItem.title }}
+  template: `
+    {{ todoItem.title }}
   `,
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
   @Input() todoItem: any;
 
   constructor() { }
