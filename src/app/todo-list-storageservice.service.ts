ca322c677c7e44df4770488843608403079eb149
Verenasworld <verena.felizidas@gmail.com>
2020-02-10 23:09:27 +0100

    build lokal storage sercive

--- src/app/todo-list-storageservice.service.ts
+++ src/app/todo-list-storageservice.service.ts
@@ -9,16 +9,32 @@ export class TodoListStorageserviceService {
   constructor() { 
     this.todoList = JSON.parse(localStorage.getItem(storageName));
   }
+
+  private update() {
+    localStorage.setItem(storageName, JSON.stringify(this.todoList));
+
+    return this.get();
+  }
+  private findItemIndex(item) {
+    return this.todoList.indexOf(item);
+  }
 // get items
-  get() {}
-
+ get() {
+    return [...this.todoList];
+  }
   // add a new item
-  post(item) {}
-
-  // update an item
-  put(item, changes) {}
-
+   post(item) {
+    this.todoList.push(item);
+    return this.update();
+  }
+put(item, changes) {
+    Object.assign(this.todoList[this.findItemIndex(item)], changes);
+    return this.update();
+  }
   // remove an item
-  destroy(item) {}
+  destroy(item) {
+    this.todoList.splice(this.findItemIndex(item), 1);
+    return this.update();
+  }
 
 }
